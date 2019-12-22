65aa91a8be5ab0f76b407c39b7fa29b16ec4afca
Alex Petrov <alex.petrov@nktech.eu>
2019-12-22 17:35:56 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AirplaneDatabase.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AirplaneDatabase.exe"
+  "AirplaneDatabase.pdb"
+  "CMakeFiles/AirplaneDatabase.dir/main.cpp.o"
+  "libAirplaneDatabase.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AirplaneDatabase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AirplaneDatabase.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AirplaneDatabase.
+# This may be replaced when dependencies are built.
